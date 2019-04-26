2ff678c074af1d2391d7e5261001bbf0be5d006c
AJ Ruf <z0isch@gmail.com>
2019-04-26 17:21:01 -0400

    added some tests
    simplify
    
    clean up

--- test/Main.purs
+++ test/Main.purs
@@ -4,10 +4,12 @@ import Prelude
 
 import Effect (Effect)
 import Test.Route as Route
+import Test.RouteQuickCheck as RouteQuickCheck
 import Test.Text as Text
 import Test.Unit.Main (runTest)
 
 main :: Effect Unit
 main = runTest do
-  Text.allTests
-  Route.allTests
+    Text.allTests
+    Route.allTests
+    RouteQuickCheck.allTests  
